a1aa2b19d227aff2c1e11cae580a2a8bfc63d96a
Mohith7548 <mohith7548@gmail.com>
2019-03-30 10:03:02 +0530

    Added requirements.txt and removed try, catch in main program.

--- zero-key-score.py
+++ zero-key-score.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
-try:
-	import pyxhook, pyperclip
-except:
-	print('Please install pyxhook and pyperclip packages!')
-	exit(0)
-
 import os
 import time
+import pyxhook
 import datetime
+import pyperclip
 import multiprocessing
 
 
